b584c97279d994bf7185b8abf2438a29d417ef6e
KyiThantSin <kyits485@gmail.com>
2023-09-20 17:06:57 +0630

    emotion setup & icon

---
+++ public/icons/IssueIcon.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function IssueIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      fill="none"
+      viewBox="0 0 30 30"
+    >
+      <path
+        stroke="#2F4858"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeOpacity="0.8"
+        strokeWidth="1.5"
+        d="M15 12.188v4.687M13.377 4.686L3.067 22.497a1.875 1.875 0 001.623 2.815h20.62a1.874 1.874 0 001.623-2.815L16.623 4.686a1.875 1.875 0 00-3.246 0v0z"
+      ></path>
+      <path
+        fill="#2F4858"
+        fillOpacity="0.8"
+        d="M15 22.5a1.406 1.406 0 100-2.812 1.406 1.406 0 000 2.812z"
+      ></path>
+    </svg>
+  );
+}
+
+export default IssueIcon;
