cebdc5b53cbb6fb8f2e59d6bcd60e984ace262b8
Zin-Mar-Win <awin@nexbe.sg>
2023-09-21 17:59:21 +0630

    update_layout_and_nav

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -20,43 +20,49 @@ const RenderNavLinkItems = ({ href, label, icon }) => {
 const Navbar = () => {
   const icons = [
     {
-      id:0,
-      href:"/",
-      label:"Home",
-      icon: <HomeIcon />
+      id: 0,
+      href: "/",
+      label: "Home",
+      icon: <HomeIcon />,
     },
     {
-      id:1,
-      href:"/emergency",
-      label:"Emergency",
-      icon: <EmergencyIcon />
+      id: 1,
+      href: "/emergency",
+      label: "Emergency",
+      icon: <EmergencyIcon />,
     },
     {
-      id:2,
-      href:"/issues",
-      label:"Issues",
-      icon: <IssueIcon />
+      id: 2,
+      href: "/issues",
+      label: "Issues",
+      icon: <IssueIcon />,
     },
     {
-      id:3,
-      href:"/patrol",
-      label:"Patrol",
-      icon: <PatrolIcon />
+      id: 3,
+      href: "/patrol",
+      label: "Patrol",
+      icon: <PatrolIcon />,
     },
     {
-      id:4,
-      href:"/more",
-      label:"More",
-      icon: <MoreIcon />
+      id: 4,
+      href: "/more",
+      label: "More",
+      icon: <MoreIcon />,
     },
-  ]
+  ];
   return (
     <nav css={styles.wrapper}>
-      {icons && icons.map((icon) => {
-        return (
-          <RenderNavLinkItems href={icon.href} label={icon.label} icon={icon.icon} key={icon.id}/>
-        )
-      })}
+      {icons &&
+        icons.map((icon) => {
+          return (
+            <RenderNavLinkItems
+              href={icon.href}
+              label={icon.label}
+              icon={icon.icon}
+              key={icon.id}
+            />
+          );
+        })}
     </nav>
   );
 };
@@ -65,24 +71,21 @@ export default Navbar;
 
 const styles = {
   wrapper: css`
-    display: flex;
-    flex-direction: row;
-    gap: 30px;
-    justify-content: center;
-    align-items: center;
-    background: #fff;
-    font-color: #2f4858;
-    padding: 20px;
-    box-shadow: 1px 1px 5px 3px rgba(0, 0, 0, 0.08);
-    border-radius: 0px 0px 30px 30px;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-
     @media (min-width: 440px) {
       width: 100%;
       justify-content: space-evenly;
     }
+    display: flex;
+    flex-direction: row;
+    height: 55px;
+    text-align: center;
+    justify-content: space-evenly;
+    background: #fff;
+    box-shadow: 1px 1px 5px 3px rgba(0, 0, 0, 0.08);
+    border-radius: 0 0 20px 20px;
+    margin-top: auto;
+    gap: 30px;
+    width: 100%;
   `,
   navLink: css`
     display: flex;
