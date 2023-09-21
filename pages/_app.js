fb762c0957681e469f4ddd5f838718b605c421ea
SrgeiHein <hein@nexbe.sg>
2023-09-21 12:44:34 +0630

    global js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
+import GlobalStyle from "../styles/globals";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+ 
 }
 
 export default MyApp
