462b79a5d2625ea3e16bc1115365c75d0411d51b
Zin-Mar-Win <awin@nexbe.sg>
2023-09-22 11:41:06 +0630

    add_routes

---
+++ pages/documents/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+
+const index = () => {
+  return <Layout>Documents</Layout>;
+};
+
+export default index;
---
+++ pages/profile/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+
+const index = () => {
+  return <Layout>Profile</Layout>;
+};
+
+export default index;
