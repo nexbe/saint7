999bed7b5303533b6ae639c45c59ba1078be59c5
Zin-Mar-Win <awin@nexbe.sg>
2023-09-22 17:35:41 +0630

    Add_Icons

---
+++ public/icons/arrowUpIcon.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="23"
+      height="22"
+      fill="none"
+      viewBox="0 0 23 22"
+    >
+      <path
+        stroke="#5C5C5C"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M17.472 14.115L11.646 8.19 5.82 14.115"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
