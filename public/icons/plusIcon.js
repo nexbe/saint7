30de1f5f30e58c8b0bff2a3dc4124782360ee179
Zin-Mar-Win <awin@nexbe.sg>
2023-09-26 15:35:20 +0630

    add_icons

---
+++ public/icons/plusIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="45"
+      height="45"
+      fill="none"
+      viewBox="0 0 45 45"
+    >
+      <path
+        fill="#fff"
+        d="M35.574 24.296H24.431v11.142h-3.714V24.296H9.574V20.58h11.143V9.438h3.714v11.143h11.143v3.715z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
