764e3ef6dff7ba07a47c3b5094ce30297b0c7df4
KyiThantSin <kyits485@gmail.com>
2023-09-26 16:09:51 +0630

    payslip-ui

---
+++ components/payslip/history/History.js
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import React from "react";
+import { css } from "@emotion/react";
+import Card from "./Card";
+
+const History = () => {
+  return (
+    <div css={styles.wrapper}>
+      <Card data={"August"} />
+      <Card data={"July"} />
+      <Card data={"June"} />
+      <Card data={"May"} />
+    </div>
+  );
+};
+
+export default History;
+
+const styles = {
+  wrapper: css`
+    display: flex;
+    flex-direction: column;
+    gap: 9px;
+    max-height: 68vh;
+    overflow-y: scroll;
+
+    @media (min-width: 440px) {
+      margin: 30px;
+    }
+  `,
+};
