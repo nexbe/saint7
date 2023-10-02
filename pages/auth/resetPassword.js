466c9113a437ae75e7d27a72bc6c8416482df34e
KyiThantSin <kyits485@gmail.com>
2023-10-02 15:31:52 +0630

    reset-password

---
+++ pages/auth/resetPassword.js
@@ -0,0 +1,153 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const resetPassword = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (email) {
+      router.push("/auth/verification");
+    }
+  };
+
+  const cusotmerSupportHandler = (e) => {
+    const email = "saint_customersupport.com";
+    e.preventDefault();
+    window.location.href = `mailto:${email}`;
+  };
+  return (
+    <div css={styles.wrapper}>
+      <div css={styles.info}>
+        <h3 css={styles.title}>Forgot your password?</h3>
+        <p>
+          Enter your email address and you will receive a verification code.
+        </p>
+        <form onSubmit={onSubmitHandler}>
+          <div css={styles.loginFormStyle}>
+            <div>
+              <label htmlFor="email">Your email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                aria-label="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div css={styles.actions}>
+            <div>Forgot email?</div>
+            <span>
+              <a href="#" onClick={cusotmerSupportHandler}>
+                contact customer support
+              </a>
+            </span>
+          </div>
+
+          <button id="reset" type="submit" css={styles.loginBtn}>
+            Get reset code
+          </button>
+          <div css={styles.backBtn} onClick={() => router.push("/")}>
+            Back
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default resetPassword;
+
+const styles = {
+  wrapper: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 15px;
+    background: var(--primary);
+  `,
+  info: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    p {
+      color: var(--white);
+      font-size: 16px;
+      font-weight: 400;
+      line-height: normal;
+    }
+  `,
+  title: css`
+    color: var(--white);
+    font-size: 32px;
+    font-weight: 600;
+    line-height: normal;
+  `,
+  loginBtn: css`
+    background: var(--white);
+    color: var(--primary);
+    width: 100%;
+    padding: 9px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    font-weight: 700;
+    border: 1px solid transparent;
+    font-size: 18px;
+    cursor: pointer;
+  `,
+  loginFormStyle: css`
+    margin-top: 30px;
+    margin-bottom: 20px;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    gap: 9px;
+
+    input {
+      color: #fff;
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid #fff;
+      outline: none;
+      width: 100%;
+      padding: 5px;
+    }
+    div {
+      margin-top: 5px;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 500;
+    }
+  `,
+  actions: css`
+    margin-top: 25px;
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: row;
+    gap: 9px;
+    color: #fff;
+    cursor: pointer;
+    span {
+      color: var(--blue);
+      text-decoration: underline;
+    }
+  `,
+  backBtn: css`
+    color: var(--white);
+    text-align: center;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: normal;
+    cursor: pointer;
+  `,
+};
---
+++ pages/auth/verification.js
@@ -0,0 +1,195 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const verification = () => {
+  const router = useRouter();
+  const [time, setTime] = useState({ minutes: 0, seconds: 59 });
+  const [firstDigit, setFirstDigit] = useState(null);
+  const [secDigit, setSecDigit] = useState(null);
+  const [thirdDigit, setThirdDigit] = useState(null);
+  const [fourthDigit, setFourthDigit] = useState(null);
+
+  useEffect(() => {
+    const startCounting = setInterval(() => {
+      if (time.minutes === 0 && time.seconds === 0) {
+        clearInterval(startCounting);
+        return;
+      }
+
+      if (time.seconds > 0) {
+        setTime((prev) => ({ ...prev, seconds: prev.seconds - 1 }));
+      } else {
+        setTime((prev) => ({ minutes: prev.minutes - 1, seconds: 59 }));
+      }
+    }, 1000);
+
+    return () => clearInterval(startCounting);
+  }, [time]);
+
+  const focusHandler = (event) => {
+    const currentInput = event.target;
+    const maxLength = parseInt(currentInput.getAttribute("maxLength"));
+    const currentLength = currentInput.value.length;
+
+    if (currentLength === maxLength) {
+      const nextInput = currentInput.nextElementSibling;
+      if (nextInput !== null) {
+        nextInput.focus();
+      }
+    }
+  };
+
+  const resendHandler = () => {
+    router.push("/auth/resetPassword")
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (firstDigit && secDigit && thirdDigit && fourthDigit) {
+      router.push("/home");
+    }
+  };
+
+  return (
+    <div css={styles.wrapper}>
+      <div css={styles.info}>
+        <h3 css={styles.title}>Verification</h3>
+        <p>
+        Please enter the 4-digit verification code that we have sent via the email. 
+        </p>
+      </div>
+      <form onSubmit={onSubmitHandler}>
+        <div css={styles.box}>
+          <input
+            id="first-digit"
+            type="number"
+            required
+            maxLength="1"
+            onChange={(e) => setFirstDigit(e.target.value)}
+            onInput={focusHandler}
+            min="0"
+          />
+          <input
+            id="second-digit"
+            type="number"
+            required
+            maxLength="1"
+            onChange={(e) => setSecDigit(e.target.value)}
+            onInput={focusHandler}
+            min="0"
+          />
+          <input
+            id="third-digit"
+            type="number"
+            required
+            maxLength="1"
+            onChange={(e) => setThirdDigit(e.target.value)}
+            onInput={focusHandler}
+            min="0"
+          />
+          <input
+            id="fourth-digit"
+            type="number"
+            required
+            maxLength="1"
+            onChange={(e) => setFourthDigit(e.target.value)}
+            onInput={focusHandler}
+            min="0"
+          />
+        </div>
+
+        <div
+          style={{ color: "var(--white)", margin: "9px", textAlign: "center" }}>
+          code expire in :{" "}
+          <span style={{ color: "#EB5656" }}>
+            {" "}
+            {time.minutes.toString().padStart(2, 0)} :{" "}
+            {time.seconds.toString().padStart(2, 0)}{" "}
+          </span>
+        </div>
+
+        <button id="continue" type="submit" css={styles.submitBtn}>
+          Continue
+        </button>
+      </form>
+      <div css={styles.actions}>
+        <div>Didn’t get the code?</div>
+        <span onClick={resendHandler}>Resend code</span>
+      </div>
+    </div>
+  );
+};
+
+export default verification;
+
+const styles = {
+  wrapper: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 20px;
+    background: var(--primary);
+  `,
+  info: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    p {
+      color: var(--white);
+      font-size: 16px;
+      font-weight: 400;
+      line-height: normal;
+    }
+  `,
+  title: css`
+    color: var(--white);
+    font-size: 32px;
+    font-weight: 600;
+    line-height: normal;
+  `,
+  box: css`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    input {
+      border: none;
+      font-size: 40px;
+      text-align: center;
+      background: var(--white);
+      padding: 9px;
+      width: 7vh;
+      height: 7vh;
+      border-radius: 2px;
+    }
+  `,
+  submitBtn: css`
+    background: var(--white);
+    color: var(--primary);
+    width: 100%;
+    padding: 9px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border-radius: 15px;
+    font-weight: 700;
+    border: 1px solid transparent;
+    font-size: 18px;
+    cursor: pointer;
+    
+  `,
+  actions: css`
+    margin-top: 25px;
+    margin-bottom: 25px;
+    display: flex;
+    flex-direction: row;
+    gap: 9px;
+    color: #fff;
+    cursor: pointer;
+    span {
+      color: var(--blue);
+    }
+  `,
+};
