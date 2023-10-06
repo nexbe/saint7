831beac92719243cf59e005d3a53640e69b73dd4
KyiThantSin <kyits485@gmail.com>
2023-10-06 17:00:21 +0630

    graphql-setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
 import '../styles/globals.css'
 import "bootstrap/dist/css/bootstrap.css";
 import GlobalStyle from "../styles/globals";
+import { ApolloProvider } from "@apollo/client";
+import client from '../graphql/apolloClient';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
  
 }
