b2bd35310a65746029d11b71bf18ec7bb3f514f0
KyiThantSin <kyits485@gmail.com>
2023-11-03 15:04:21 +0630

    calendar-ui

--- pages/attendance/Manager/schedule/index.js
+++ pages/attendance/Manager/schedule/index.js
@@ -1,32 +1,25 @@
 /** @jsxImportSource @emotion/react */
 import { useState, useEffect } from "react";
 import { css } from "@emotion/react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import Layout from "../../../../components/layout/Layout";
 import HeaderNoti from "../../../../components/layout/HeaderNoti";
 import ScheduleCard from "../../../../components/attendence/ScheduleCard";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Schedule = () => {
-  const [startDate, setStartDate] = useState();
+  const [dates, setDates] = useState(new Date());
+  console.log(dates)
   return (
     <Layout>
       <HeaderNoti title={"Schedule"} href={"/attendance/Manager"}/>
-      <div css={styles.caledarContainer}>
-        <div css={styles.calendarCard}>
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            dateFormat="dd/MM/yyyy"
-            open
-            formatWeekDay={(nameOfDay) => nameOfDay.substr(0, 3)}
-          />
-        </div>
-      </div>
+      <div>
+        <Calendar onChange={setDates} value={dates} css={styles.calendar} />
       <div css={styles.container}>
         <h5>16th june ,2023</h5>
         <ScheduleCard state={true} />
         <ScheduleCard state={false}/>
+      </div>
       </div>
     </Layout>
   );
@@ -35,105 +28,24 @@ const Schedule = () => {
 export default Schedule;
 
 const styles = {
-  caledarContainer: css`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    width: 100%;
-    margin-bottom:15%;
-    @media (max-width: 680px) {
-      margin-bottom:65%;
+  calendar:css`
+    width:100%;
+    border:none;
+    border-radius: 0px 0px 16px 16px;
+    .react-calendar__tile--active:enabled:hover, .react-calendar__tile--active:enabled:focus, .react-calendar__tile--active{
+      background:#386fff;
+      border-radius:9px;
     }
-  `,
-  calendarCard: css`
-    display: flex;
-    flex-direction: row;
-    line-height: 25px;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    .react-datepicker__tab-loop {
-      div {
-        inset: none;
-        transform: none;
-        padding: 0;
-      }
-    }
-    label {
-      display: flex;
-      flex-direction: column;
-    }
-    .react-datepicker-wrapper {
-      input {
-        display: none;
-      }
-    }
-    .react-datepicker__triangle {
-      display: none;
-    }
-    .react-datepicker {
-      border-radius: 0px 0px 16px 16px;
-      background: var(--white);
-      box-shadow: 0px 4px 8px 0px rgba(21, 21, 21, 0.15);
-      border: none;
-      margin-top: -25px;
-      padding-left: 10px;
-      @media (max-width: 450px) {
-        width: 100vw;
-      }
-      @media (min-width: 450px) {
-        width: 100%;
-        justify-content: center;
-      }
-    }
-    .react-datepicker__header {
-      background: var(--white);
-      border: none;
-      display: flex;
-      flex-direction: column;
-      gap: 10px;
-      width: 100%;
-      align-items: center;
-      @media (min-width: 390px) {
-        margin-left: 10px;
-      }
-    }
-    .react-datepicker__week,
-    .react-datepicker__day-names {
-      white-space: nowrap;
-      display: flex;
-      gap: 20px;
-      text-transform: uppercase;
-      align-items: center;
-      @media (min-width: 390px) {
-        margin-left: 10px;
-      }
-    }
-    .react-datepicker__current-month {
-      color: var(--primary);
-      font-family: Inter;
-      font-size: 18px;
-      font-style: normal;
-      font-weight: 600;
-      line-height: normal;
-      margin-top: 15px;
-    }
-    .react-datepicker__navigation-icon--next {
-      top: 11px;
-      left: -10px;
-      font-size: 16px;
-    }
-    .react-datepicker__navigation-icon--previous {
-      top: 11px;
-      left: 5px;
-      font-size: 16px;
+    .react-calendar__tile--now{
+      border-radius:9px;
     }
   `,
   container:css`
      background:#fff;
+     border-radius: 10px;
+     margin-top:10px;
      padding:20px;
-     max-height:50vh;
+     max-height:46vh;
      overflow-y:scroll;
      h5{
       color: #2F4858;
@@ -141,7 +53,7 @@ const styles = {
       font-weight: 600;
      }
      @media (max-width: 375px) {
-      max-height:35vh;
+      max-height:30vh;
     }
   `
 };
