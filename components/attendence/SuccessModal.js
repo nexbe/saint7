3a80be3a8ffae7fb46a1cf40b3072527df791644
KyiThantSin <kyits485@gmail.com>
2023-11-23 23:51:19 +0630

    fix-issue

--- components/attendence/SuccessModal.js
+++ components/attendence/SuccessModal.js
@@ -10,9 +10,7 @@ const SuccessModal = ({ isOpen, setModal, handleSubmit, count }) => {
     <Modal isOpen={isOpen} toggle={close} css={styles.wrapper}>
       <div css={styles.content}>
         <p>Congratulations! </p>
-        <p>
-          <b>{count} users</b> has been created successfully.
-        </p>
+        <p><b>{count} users</b> has been created successfully.</p>
       </div>
       <div css={styles.btn} onClick={handleSubmit}>OK</div>
     </Modal>
@@ -22,15 +20,13 @@ const SuccessModal = ({ isOpen, setModal, handleSubmit, count }) => {
 export default SuccessModal;
 const styles = {
   wrapper: css`
-    margin-top: 50%;
-    padding: 20px;
+    margin-top: 60%;
+    padding: 15px;
     border-radius: 16px;
     background: #fff;
     color: var(--primary-font);
     box-shadow: -1px 1px 4px 0px rgba(0, 0, 0, 0.08);
-    p {
-      margin: 20px;
-    }
+   
     .modal-content {
       border: none;
     }
@@ -41,14 +37,14 @@ const styles = {
   content: css`
     color: #2f4858;
     display: flex;
+    font-size: 16px;
     flex-direction: column;
     text-align: center;
     justify-content: center;
-    align-items: center;
   `,
   btn: css`
     margin-left: auto;
-    font-size: 16px;
+    font-size: 18px;
     font-weight: 700;
   `,
 };
