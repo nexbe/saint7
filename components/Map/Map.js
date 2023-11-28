ed9c78f19493e2faad88402217fb22880fa13377
SrgeiHein <hein@nexbe.sg>
2023-11-28 22:46:30 +0630

    map issues

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -4,27 +4,21 @@ import styles from "../../styles/Home.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import attendenceStore from "../../store/attendance";
-import { parseCookies } from "nookies";
 
 const Map = () => {
   const { locationData: locationData, addressData } = attendenceStore(
     (state) => state
   );
-  const cookies = parseCookies();
-
   const customIcon = new L.Icon({
     iconUrl: "/images/mapIcon.png", // Path to your custom icon image
     iconSize: [50, 50], // Size of the icon
   });
 
-  const latitude = cookies.latitude ? JSON.parse(cookies.latitude) : null;
-  const longitude = cookies.longitude ? JSON.parse(cookies.longitude) : null;
-
   return (
     <MapContainer
       className={styles.map}
-      center={[latitude, longitude]}
-      zoom={10}
+      center={[locationData?.lat, locationData?.lng]}
+      zoom={17}
     >
       <TileLayer
         attribution="Google Maps"
@@ -33,9 +27,11 @@ const Map = () => {
         subdomains={["mt0", "mt1", "mt2", "mt3"]}
       />
       <Marker
-        position={[latitude ?? "16.871311", longitude ?? "96.199379"]}
+        position={[locationData?.lat, locationData?.lng]}
         icon={customIcon}
-      ></Marker>
+      >
+        <Popup>{addressData}</Popup>
+      </Marker>
     </MapContainer>
   );
 };
