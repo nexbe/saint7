cf7c204926adbf72aafd48f32858f0382492d247
SrgeiHein <hein@nexbe.sg>
2023-11-28 23:18:00 +0630

    testing

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -18,7 +18,7 @@ const Map = ({ lat, lng }) => {
   });
 
   return (
-    <MapContainer className={styles.map} center={[lat, lng]} zoom={17}>
+    <MapContainer className={styles.map} center={[lat, lng]} zoom={15}>
       <TileLayer
         attribution="Google Maps"
         url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
